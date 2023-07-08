09ec201dd3faf129fbf46c85b0d16991d6e6ae56
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-07-08 17:24:51 +0530

    Added solution - LeetHub

---
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaxSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int findMaxSum(int arr[], int n) {
+        // code here
+        int dp[] = new int[n];
+        dp[0]=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            int pick = arr[i];
+            if(i>1)
+             pick =pick + dp[i-2];
+            int npick = 0 + dp[i-1];
+            
+            dp[i]=Math.max(pick,npick);
+            
+        }
+        return dp[n-1];
+        
+    }
+}
