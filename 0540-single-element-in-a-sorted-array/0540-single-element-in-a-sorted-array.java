1f2812c07088b3131d9ac508dd81e685a6b3b029
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-07-28 14:22:29 +0530

    Time: 13 ms (5.03%), Space: 54.7 MB (6.24%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+         Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int key:map.keySet())
+        {
+            if(map.get(key)==1)
+            return key;
+        }
+       
+     return -1;
+    }
+}
