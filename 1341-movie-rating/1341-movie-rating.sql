94230ce2e635081a5beed4a8e0a51a1674378ec3
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-08-04 09:52:09 +0530

    Time: 2605 ms (71.47%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,6 +1,6 @@
 SELECT results
 FROM (
-    SELECT IFNULL(u.name, '') AS results, COUNT( r.movie_id) AS num_movies
+    SELECT u.name AS results, COUNT( r.movie_id) AS num_movies
     FROM Users u
     inner JOIN MovieRating r ON u.user_id = r.user_id
     GROUP BY u.user_id
@@ -10,7 +10,7 @@ FROM (
 UNION all
 SELECT results
 FROM (
-    SELECT IFNULL(m.title, '') AS results, AVG(r.rating) AS avg_rating
+    SELECT m.title AS results, AVG(r.rating) AS avg_rating
     FROM MovieRating r
     JOIN Movies m ON r.movie_id = m.movie_id
     WHERE YEAR(r.created_at) = 2020 AND MONTH(r.created_at) = 2
