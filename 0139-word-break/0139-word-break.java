efe28bed77cedd115fdec11f8fc88fe159471051
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-08-04 10:53:03 +0530

    Time: 4 ms (76.66%), Space: 41.4 MB (81.12%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,14 +3,15 @@ class Solution {
         int n = s.length();
         boolean dp[] = new boolean[n+1];
         dp[n]=true;
+        
         for(int i=n-1;i>=0;i--)
         {
             for(String wrd:wordDict)
             {
                 if(i+wrd.length()<=n && wrd.equals(s.substring(i,i+wrd.length())))
-                     {
-                    dp[i]=dp[i+wrd.length()]; 
-                     }
+                {
+                    dp[i]=dp[i+wrd.length()];
+                }
                 if(dp[i])
                     break;
             }
