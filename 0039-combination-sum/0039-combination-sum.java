d4889d61210ce878c857cb6fdeaa53dca53d3cdb
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-08-07 21:53:20 +0530

    Time: 2 ms (79.04%), Space: 43.5 MB (98.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        combination(res,temp,0,candidates,target);
-        return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        int sum =0;
+        comb(candidates,target,0,candidates.length,ans,list,sum);
+        return ans;
     }
-    void combination(List<List<Integer>> res, List<Integer> list, int idx, int can[], int sum)
+    void comb(int candidates[], int target,int idx,int n, List<List<Integer>> ans, List<Integer> list,int sum)
     {
-        
-        if(sum==0)
+        if(idx==n)
         {
-            res.add(new ArrayList<>(list));
+        if(target==sum)
+        {
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=idx;i<can.length;i++)
-        {
-            if(sum-can[i]>=0)
-            {
-                list.add(can[i]);
-                combination(res,list,i,can,sum-can[i]);
-                list.remove(list.size()-1);
-            }        
+        return;
         }
+        else if(sum>target)
+            return;
+        list.add(candidates[idx]);
+        comb(candidates,target,idx,n,ans,list,sum+candidates[idx]);
+        list.remove(list.size()-1);
+        comb(candidates,target,idx+1,n,ans,list,sum);
         
-        }
-    
     }
-    
+}
