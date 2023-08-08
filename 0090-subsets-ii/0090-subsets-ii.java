4e3b4e895b475d4f052d37e0fb39adc759f9be8f
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-08-08 11:14:16 +0530

    Time: 11 ms (8.36%), Space: 46.1 MB (10.18%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -3,23 +3,32 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        subset(0,nums,list,ans);
-         return ans;
         
+        subset(nums,0,list,ans);
+        HashSet<List<Integer>> set = new HashSet<>();
+        for(int i=0;i<ans.size();i++)
+        {
+            set.add(ans.get(i));
+        }
+        List<List<Integer>> val = new ArrayList<>(set);
+        return val;
     }
-    void subset(int idx, int nums[], List<Integer> list,List<List<Integer>> ans)
+    void subset(int nums[], int idx , List<Integer> list, List<List<Integer>> ans)
     {
-        ans.add(new ArrayList<>(list));
+        if(idx == nums.length)
+        {
+            ans.add(new ArrayList<>(list));
+            
+        }
         
         for(int i=idx;i<nums.length;i++)
         {
-            if(i!=idx && nums[i]==nums[i-1])
+            if(i>idx && nums[i]==nums[i-1])
                 continue;
             list.add(nums[i]);
-            subset(i+1,nums,list,ans);
+            subset(nums,i+1,list,ans);
             list.remove(list.size()-1);
-            
-        }
-       
+            subset(nums,i+1,list,ans);
+            }
     }
 }
