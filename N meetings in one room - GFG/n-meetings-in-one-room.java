744e2a02d66170f0e54e4c81cebfec532a7b42e6
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-08-18 23:43:43 +0530

    Added solution - LeetHub

---
+++ N meetings in one room - GFG/n-meetings-in-one-room.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++)
+                start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                end[i] = Integer.parseInt(inputLine[i]);
+                
+            int ans = new Solution().maxMeetings(start, end, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        // add your code here
+        
+
+        int[][] arr = new int[n][2];  // Declare and initialize as a 2D array
+        
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = start[i];
+            arr[i][1] = end[i];
+        }
+        
+        Arrays.sort(arr, Comparator.comparingInt((int[] a) -> a[1])
+                          .thenComparingInt(a -> a[0]));
+
+        int count = 1;
+        int i = 1;
+        int j = 0;
+        while (i < n) {
+            if (arr[i][0] > arr[j][1]) {
+                count++;
+                j = i;
+            }
+            i++;
+        }
+
+        return count;
+    }
+}
