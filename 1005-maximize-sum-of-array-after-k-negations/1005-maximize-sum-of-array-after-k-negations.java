22ca5ea07057d0b59d718909e51cf3531cdbd476
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-08-19 19:05:32 +0530

    Time: 3 ms (68.74%), Space: 41.1 MB (81.05%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
@@ -1,38 +1,33 @@
+
+
 class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
         Arrays.sort(nums);
-        int i=0;
-        while(k!=0)
-        {
-            while(nums[i]<0 && k!=0)
-            {
-                nums[i]=-nums[i];
-                k--;
-                Arrays.sort(nums);
-            }
-            if(k==0)
-            {
-                break;
-            }
-           
-            
-             if(nums[i]==0)
-            {
-                nums[i]=-nums[i];
-                k--;
-            }
-            else
-            {
-                nums[i]=-nums[i];
-                k--;
-            }
-            
+
+        int i = 0;
+
+        // Negate negative numbers while there are still negative numbers and k > 0
+        while (k > 0 && i < nums.length && nums[i] < 0) {
+            nums[i] = -nums[i];
+            k--;
+            i++;
         }
-        int sum =0;
-        for(i=0;i<nums.length;i++)
-        {
-            sum+=nums[i];
+
+        // If k is still > 0 and it's odd, we will negate the smallest remaining element
+        if (k > 0 && k % 2 == 1) {
+            Arrays.sort(nums); // Sort again to make sure the smallest element is at the beginning
+            nums[0] = -nums[0];
+        }
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
         return sum;
     }
 }
+
+
+
+
+
