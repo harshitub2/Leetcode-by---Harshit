d5ed6964fc35973a69607eb361cdb2aabb84d60e
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-09-05 08:35:36 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (9.53%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,36 +15,31 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Map<Node,Node> map= new HashMap<>();
-        Node temp = head;
-        while(temp!=null)
+        Map<Node,Node> map = new HashMap<>();
+        Node curr = head;
+        while(curr!=null)
         {
-            Node node = new Node(temp.val);
-            map.put(temp,node);
-            temp=temp.next;
+            Node node = new Node(curr.val);
+            map.put(curr,node);
+            curr=curr.next;
         }
-        temp=head;
-        while(temp!=null)
+        curr= head;
+        while(curr!=null)
         {
-           Node node = map.get(temp);
-            if(temp.next!=null)
+            if(curr.next!=null)
             {
-                map.get(temp).next=map.get(temp.next);
+                map.get(curr).next=map.get(curr.next);
+               
             }
             else
-            {
-               map.get(temp).next=null;
-            }
-            if(temp.random!=null)
-            {
-                map.get(temp).random = map.get(temp.random);
-            }
+                map.get(curr).next=null;
+            if(curr.random!=null)
+                map.get(curr).random=map.get(curr.random);
             else
-            {
-                map.get(temp).random  = null;
-            }
-            temp=temp.next;
+                map.get(curr).random=null;
+            curr=curr.next;
         }
-        return map.get(head);
+       return map.get(head);
+        
     }
 }
