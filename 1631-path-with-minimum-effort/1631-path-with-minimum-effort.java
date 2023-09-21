032178beb6a12ee366780a22f904fc1ff0e7c6d8
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2023-09-21 10:20:24 +0530

    Time: 43 ms (55.12%), Space: 44.1 MB (70.00%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,30 +1,43 @@
 class Solution {
-    int[] DIR = new int[]{0, 1, 0, -1, 0};
     public int minimumEffortPath(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
-        int[][] dist = new int[m][n];
-        for (int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-        
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minHeap.offer(new int[]{0, 0, 0}); // distance, row, col
-        dist[0][0] = 0;
-        
-        while (!minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int d = top[0], r = top[1], c = top[2];
-           
-            if (r == m - 1 && c == n - 1) return d; // Reach to bottom right
-            for (int i = 0; i < 4; i++) {
-                int nr = r + DIR[i], nc = c + DIR[i + 1];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                    int newDist = Math.max(d, Math.abs(heights[nr][nc] - heights[r][c]));
-                    if (dist[nr][nc] > newDist) {
-                        dist[nr][nc] = newDist;
-                        minHeap.offer(new int[]{dist[nr][nc], nr, nc});
+        int m = heights.length;
+        int n = heights[0].length;
+        int dir[][] = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+        int d[][] = new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            Arrays.fill(d[i],Integer.MAX_VALUE);
+        }
+        queue.offer(new int[]{0,0,0});
+        d[0][0]=0;
+        while(!queue.isEmpty())
+        {
+            int top[] =queue.poll();
+            int dis = top[0];
+            int row = top[1];
+            int col = top[2];
+            if(row==m-1 && col==n-1)
+                return dis;
+            for(int i=0;i<4;i++)
+            {
+                int x = row + dir[i][0];
+                int y = col + dir[i][1];
+                
+                if(x>=m || x<0 || y>=n || y<0)
+                    continue;
+                int newDist = Math.max(dis,Math.abs(heights[row][col]-heights[x][y]));
+                    if(d[x][y]>newDist)
+                    {
+                        d[x][y]=newDist;
+                        queue.offer(new int[]{d[x][y],x,y});
                     }
                 }
+                
             }
+        
+        return -1;
         }
-        return 0; // Unreachable code, Java require to return interger value.
-    }
+        
+    
 }
