733e97306c7e5104c95102c4dc9168817d4e7eb8
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2024-08-15 10:10:23 +0530

    Time: 16 ms (5.52%), Space: 58.1 MB (5.11%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,57 +1,42 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int n = bills.length;
-        int arr[] = new int[3];
-        
-        
-            for(int i=0;i<n;i++)
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(5,0);
+        map.put(10,0);
+        map.put(20,0);
+        for(int i =0;i<bills.length;i++)
+        {
+            if(bills[i]==5)
             {
-            if(bills[i]-5==0)
-            {
-                
-                arr[0]++;
-                
+                map.put(5,map.get(bills[i])+1);
             }
-            else if(bills[i]-5==5)
+            else if(bills[i]==10)
             {
-                arr[1]++;
-                if(arr[0]<1)
+                if(map.get(5)>0)
                 {
-                    return false;
+                    map.put(5,map.get(5)-1);
+                    map.put(10,map.get(10)+1);
                 }
                 else
-                {
-                arr[0]--;
-                    
-                }
+                    return false;
             }
-                else if(bills[i]-5==15)
+            else if(bills[i]==20)
+            {
+                if(map.get(5)>0 && map.get(10)>0)
                 {
-                    if(arr[1]>0)
-                    {
-                        arr[1]--;
-                        if(arr[0]>0)
-                        {
-                            arr[0]--;
-                            continue;
-                        }
-                        else 
-                            return false;
-                    }
-                    else
-                    {
-                        if(arr[0]<3)
-                        {
-                            return false;
-                        }
-                        else
-                        {
-                            arr[0]=arr[0]-3;
-                        }
-                    }
-                    
+                    map.put(5,map.get(5)-1);
+                    map.put(10,map.get(10)-1);
+                    map.put(20,map.get(20)+1); 
                 }
+                else if(map.get(5)>=3)
+                {
+                    map.put(5,map.get(5)-3);
+                     map.put(20,map.get(20)+1); 
+                }
+                else
+                    return false;
             }
+        }
         return true;
     }
 }
