b0d4117183645e2644f5f6127026d653d0bba29a
Harshit jaiswal <87989425+harshitub2@users.noreply.github.com>
2024-09-18 08:02:22 +0530

    Time: 5 ms (98.24%), Space: 42.8 MB (85.65%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,15 +1,35 @@
+
+
 class Solution {
     public String largestNumber(int[] nums) {
-        String s[]=new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            s[i]=String.valueOf(nums[i]);
+        // Convert the integers to strings
+        String[] numStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numStrs[i] = String.valueOf(nums[i]);
         }
-        String ans = "";
-        Arrays.sort(s,(a,b)->(b+a).compareTo(a+b));
-        for(String str:s)
-        {
-            ans+=str;
+
+        // Sort the array with a custom comparator that compares concatenated results
+        Arrays.sort(numStrs, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                // We need to sort in descending order, so reverse the comparison
+                return order2.compareTo(order1);
+            }
+        });
+
+        // If the largest number is '0', the result should be '0'
+        if (numStrs[0].equals("0")) {
+            return "0";
         }
-       return ans.charAt(0)!='0'?ans:"0";
+
+        // Build the largest number from the sorted array
+        StringBuilder largestNumber = new StringBuilder();
+        for (String numStr : numStrs) {
+            largestNumber.append(numStr);
+        }
+
+        return largestNumber.toString();
     }
 }
